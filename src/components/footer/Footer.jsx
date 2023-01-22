0940b6cd2a0fe0bb189b51c0257113c7acdaea99
LucasBelgamann <lucasbelgamann@hotmail.com>
2023-01-21 21:34:03 -0300

    feat: static forms

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,21 +7,35 @@ function Footer() {
   return (
     <div>
       <div className="container-form">
-        <form className="form">
+        <form
+          className="form"
+          action="https://api.staticforms.xyz/submit"
+          method="post"
+        >
           <h2 id="footer">Formulário para contato</h2>
-          <input type="text" placeholder="Type your name"/>
-          <input type="email" placeholder="Type your email" />
-          <textarea placeholder="Type your message" />
+          <input
+            type="hidden"
+            name="accessKey"
+            value="a90e6195-838c-47e5-90c0-80ba0cda36fd"
+          />
+          <input
+            type="hidden"
+            name="redirectTo"
+            value="https://my-portfolio-git-main-lucasbelgamann.vercel.app/"
+          ></input>
+          <input type="text"  name="name" placeholder="Digite seu nome" />
+          <input type="email" name="email" placeholder="Type your email" />
+          <textarea placeholder="Digite sua menssagem" name="message" />
           <button type="submit">Enviar</button>
-          <button type="submit">
-          <a href="/curriculo.pdf" download>Download CV</a>
-          </button>
         </form>
-       <div className="links">
+        <div className="links">
           <a href="https://github.com/LucasBelgamann">
             <img src={git} alt="github" />
           </a>
-          <a href="https://www.linkedin.com/in/lucasbelgamann/" className="linked">
+          <a
+            href="https://www.linkedin.com/in/lucasbelgamann/"
+            className="linked"
+          >
             <img src={linkedin} alt="linkedin" />
           </a>
           <a href="https://www.instagram.com/lucasag0stinh0/">
